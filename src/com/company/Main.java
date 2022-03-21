2831789155a4438c9d49744247af0422d9c76499
Nerfay <antonishen68@gmail.com>
2022-03-21 09:44:49 +0200

    task26 is done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,7 +10,12 @@ public class Main {
           task126();
     }
     private static void task26(){
-
+        int n = (int) (Math.random() * 10 + 1);
+        float k= 1.0F;
+        for (int i = 2; i <= 2*n ; i+=2) {
+            k*=(float) i/(i+1);
+        }
+        System.out.println(k+"\nn="+n);
     }
     private static void task51(){
 
