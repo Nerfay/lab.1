83545639524e59035a76764c294bb4385f68f435
Nerfay <antonishen68@gmail.com>
2022-03-21 08:31:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+          task26();
+          task51();
+          task76();
+          task101();
+          task126();
+    }
+    private static void task26(){
+
+    }
+    private static void task51(){
+
+    }
+    private static void task76(){
+
+    }
+    private static void task101(){
+
+    }
+    private static void task126(){
+
+    }
+}
